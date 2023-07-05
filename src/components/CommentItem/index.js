4f865b7aa026226322413e6e778d978614d2da31
VineelaMuthyala <131747823+VineelaMuthyala@users.noreply.github.com>
2023-07-05 22:58:20 +0530

    Update index.js

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -1,13 +1,27 @@
 import './index.css'
 
 const CommentItem = props => {
-  const {commentDetails} = props
-  const {name, comment, colour} = commentDetails
+  const {commentDetails, deleteTheComment, toggleSwitch} = props
+  const {name, comment, colour, id, isLiked} = commentDetails
   const initialLetter = name[0]
   console.log(name)
+
+  const deleteComment = () => {
+    deleteTheComment(id)
+  }
+
+  const likeButtonClicked = () => {
+    toggleSwitch(id)
+  }
+  const likeIcon = isLiked
+    ? 'https://assets.ccbp.in/frontend/react-js/comments-app/liked-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png'
+
+  const likeColour = isLiked ? 'like-text' : 'like-text-blue'
+
   return (
     <div className="comment-item-container">
-      <li>
+      <li className="list-container">
         <div className="name-container">
           <div className={colour}>{initialLetter}</div>
           <h1 className="name">{name}</h1>
@@ -21,11 +35,16 @@ const CommentItem = props => {
             <img
               className="like-icon"
               alt="like"
-              src="https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png"
+              src={likeIcon}
+              onClick={likeButtonClicked}
             />
-            <p className="like-text">Like</p>
+            <p className={likeColour}>Like</p>
           </div>
-          <button className="delete-button" type="button">
+          <button
+            className="delete-button"
+            type="button"
+            onClick={deleteComment}
+          >
             <img
               className="delete-icon"
               alt="delete"
