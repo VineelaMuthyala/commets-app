11d92729380e9b755b9de1245148a0096a7397c0
VineelaMuthyala <131747823+VineelaMuthyala@users.noreply.github.com>
2023-07-06 11:51:42 +0530

    Update index.js

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -1,4 +1,10 @@
+import {formatDistanceToNow} from 'date-fns'
 import './index.css'
+
+const date = new Date()
+console.log(date)
+
+const time = formatDistanceToNow(new Date(date))
 
 const CommentItem = props => {
   const {commentDetails, deleteTheComment, toggleSwitch} = props
@@ -14,10 +20,10 @@ const CommentItem = props => {
     toggleSwitch(id)
   }
   const likeIcon = isLiked
-    ? 'https://assets.ccbp.in/frontend/react-js/comments-app/liked-img.png'
-    : 'https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png'
+    ? 'https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/comments-app/liked-img.png'
 
-  const likeColour = isLiked ? 'like-text' : 'like-text-blue'
+  const likeColour = isLiked ? 'like-text-blue' : 'like-text'
 
   return (
     <div className="comment-item-container">
@@ -25,25 +31,25 @@ const CommentItem = props => {
         <div className="name-container">
           <div className={colour}>{initialLetter}</div>
           <h1 className="name">{name}</h1>
-          <p className="time">2 min</p>
+          <p className="time">{time}</p>
         </div>
         <div className="comment-container">
           <p className="comment-text">{comment}</p>
         </div>
         <div className="like-delete-container">
-          <div className="like-container">
-            <img
-              className="like-icon"
-              alt="like"
-              src={likeIcon}
-              onClick={likeButtonClicked}
-            />
+          <button
+            type="button"
+            className="like-container"
+            onClick={likeButtonClicked}
+          >
+            <img className="like-icon" alt="like" src={likeIcon} />
             <p className={likeColour}>Like</p>
-          </div>
+          </button>
           <button
             className="delete-button"
             type="button"
             onClick={deleteComment}
+            data-testid="delete"
           >
             <img
               className="delete-icon"
